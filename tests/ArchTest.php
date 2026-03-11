150f1e92d21ae1e3cfb6375cf07830d711f76976
Aditya Gupta <mailadityaguptaa@gmail.com>
2026-03-11 17:20:32 +0530

    test(arch): enforce package architecture constraints

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,34 @@
 <?php
 
+declare(strict_types=1);
+
+use AdityaaCodes\LaravelCheckpoint\Contracts\BackupDriver;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+arch('src has no app references')
+    ->expect('AdityaaCodes\\LaravelCheckpoint')
+    ->not->toUse('App\\');
+
+arch('package code uses strict types')
+    ->expect('AdityaaCodes\\LaravelCheckpoint')
+    ->toUseStrictTypes();
+
+arch('contracts are interfaces')
+    ->expect('AdityaaCodes\\LaravelCheckpoint\\Contracts')
+    ->toBeInterfaces();
+
+arch('events are readonly')
+    ->expect('AdityaaCodes\\LaravelCheckpoint\\Events')
+    ->toBeReadonly();
+
+arch('jobs implement should queue')
+    ->expect('AdityaaCodes\\LaravelCheckpoint\\Jobs')
+    ->toImplement(ShouldQueue::class);
+
+arch('drivers implement backup driver')
+    ->expect('AdityaaCodes\\LaravelCheckpoint\\Drivers')
+    ->toImplement(BackupDriver::class);
+
 arch('it will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
