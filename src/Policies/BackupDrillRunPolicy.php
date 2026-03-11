fdc96df4b2f6a60d3f75a36c3e32b0942a766da7
Aditya Gupta <mailadityaguptaa@gmail.com>
2026-03-11 17:13:04 +0530

    feat(auth): add backup drill run policy

---
+++ src/Policies/BackupDrillRunPolicy.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdityaaCodes\LaravelCheckpoint\Policies;
+
+use AdityaaCodes\LaravelCheckpoint\Models\BackupDrillRun;
+
+class BackupDrillRunPolicy
+{
+    public function viewAny(object $user): bool
+    {
+        return true;
+    }
+
+    public function view(object $user, BackupDrillRun $backupDrillRun): bool
+    {
+        return true;
+    }
+
+    public function create(object $user): bool
+    {
+        return false;
+    }
+
+    public function update(object $user, BackupDrillRun $backupDrillRun): bool
+    {
+        return false;
+    }
+
+    public function delete(object $user, BackupDrillRun $backupDrillRun): bool
+    {
+        return false;
+    }
+}
