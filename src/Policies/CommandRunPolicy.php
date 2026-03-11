a975f2b61dc5961fac1d9dcb531b8b4aad653740
Aditya Gupta <mailadityaguptaa@gmail.com>
2026-03-11 17:13:17 +0530

    feat(auth): add command run policy

---
+++ src/Policies/CommandRunPolicy.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdityaaCodes\LaravelCheckpoint\Policies;
+
+use AdityaaCodes\LaravelCheckpoint\Models\CommandRun;
+
+class CommandRunPolicy
+{
+    public function viewAny(object $user): bool
+    {
+        return true;
+    }
+
+    public function view(object $user, CommandRun $commandRun): bool
+    {
+        return true;
+    }
+
+    public function create(object $user): bool
+    {
+        return true;
+    }
+}
